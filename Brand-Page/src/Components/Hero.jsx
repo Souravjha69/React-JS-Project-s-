6de05d9c0ba3ff7a93d48fc82d613cd778b999c3
Souravjha69 <souravkumarjha301@gmail.com>
2024-03-17 20:47:29 +0530

    commit changes in Brand Project

---
+++ Brand-Page/src/Components/Hero.jsx
@@ -0,0 +1,31 @@
+function Herosection() {
+  return <>
+  <main className="hero container">
+    <div className="hero-content">
+      <h1>YOUR FEET DESERVE THE BEST</h1>
+      <p>YOUR FEET DESERVE THE BEST AND WE ARE HERE TO HELP YOU WITH YOUR SHOES.
+      YOUR FEET DESERVE THE BEST AND WE ARE HERE TO HELP YOU WITH YOUR SHOES.
+      </p>
+
+      <div className="hero-btn">
+        <button>Shop Now</button>
+        <button className="Secondary-btn">Category</button>
+      </div>
+
+      <div className="Shopping">
+        <p>Also Available On</p>
+      </div>
+
+      <div className="Brand-logo">
+        <img src="/Images/Amazon.png" alt="Amazon"></img>
+        <img src="/Images/Flipkart.png" alt="Flipkart"></img>
+      </div>
+    </div>
+    <div className="hero-image">
+    <img src="/Images/Shoe.png" alt="Amazon"></img>
+    </div>
+  </main>
+  </>;
+}
+
+export default Herosection;
---
+++ Brand-Page/src/Components/Navigation.jsx
@@ -0,0 +1,24 @@
+
+function Navigation () {
+  return (
+    <>
+    <nav className="container">
+        <div className="logo">
+          <img src="/Images/download.png" alt="Logo"></img>
+        </div>
+        <ul>
+          <li href="#">Menu</li>
+          <li href="#">Location</li>
+          <li href="#">About</li>
+          <li href="#">Contact</li>
+        </ul>
+
+        <button>
+          Login
+        </button>
+      </nav>
+    </>
+  )
+}
+
+export default Navigation;
